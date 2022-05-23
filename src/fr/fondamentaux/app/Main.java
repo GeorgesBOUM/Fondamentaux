a755ac4ef0612cdb1876dffe4257bdc3c5f62960
BOUM Georges <37276156+GeorgesBOUM@users.noreply.github.com>
2022-05-23 21:12:34 +0200

    Ajout pseudo test Capitale

--- src/fr/fondamentaux/app/Main.java
+++ src/fr/fondamentaux/app/Main.java
@@ -6,9 +6,15 @@ public class Main {
 		Ville marseille = new Ville("Marseille", "France", 861635);
 		System.out.println(marseille);
 		System.out.println(marseille.description());
+		
 		Ville indefinie = new Ville();
 		System.out.println(indefinie);
 		System.out.println(indefinie.description());
+		
+		Capitale paris = new Capitale("Paris", "France", 2165423, "La tour eiffel");
+		System.out.println(paris);
+		System.out.println(paris.description());
+		
 		System.out.println("Nombre de villes créées : " + Ville.getNombreDeVillesCreees());
 	}
 
