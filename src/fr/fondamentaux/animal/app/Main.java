88c8fedcf51a06a1f4dabf709eb07ed947929862
BOUM Georges <37276156+GeorgesBOUM@users.noreply.github.com>
2022-05-25 05:03:47 +0200

    pseudo tests interface

--- src/fr/fondamentaux/animal/app/Main.java
+++ src/fr/fondamentaux/animal/app/Main.java
@@ -9,6 +9,24 @@ public class Main {
 	    l.seDeplacer();
 	    l.crier();
 	    System.out.println(l);
+	    
+	    Chien c = new Chien("Noir", 15);
+	    c.boire();
+	    c.manger();
+	    c.seDeplacer();
+	    c.crier();
+	    System.out.println("--------------------------------------------");
+	    //Les méthodes de l'interface
+	    c.faireCalin();
+	    c.faireLeBeau();
+	    c.faireLechouille();
+			
+	    System.out.println("--------------------------------------------");
+	    //Utilisons le polymorphisme de notre interface
+	    Rintintin r = new Chien();
+	    r.faireLeBeau();
+	    r.faireCalin();
+	    r.faireLechouille();
 	}
 
 }
