f193b8c9e4bcbc1fb8d88bd927d4e7f57d93c6c5
BOUM Georges <37276156+GeorgesBOUM@users.noreply.github.com>
2022-05-26 17:32:22 +0200

    Create NombreHabitantException.java

---
+++ src/fr/fondamentaux/ville/app/NombreHabitantException.java
@@ -0,0 +1,11 @@
+package fr.fondamentaux.ville.app;
+
+public class NombreHabitantException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public NombreHabitantException() {
+		System.out.println("Vous essayez d'instancier une classe Ville avec un nombre d'habitants négatif !");
+	}
+
+}
